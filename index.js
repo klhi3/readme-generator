9ce9ef24ac23a2cb61c3c1426faedd0ca05eba14
Hekwang Lhi <hekwang.lhi@gmail.com>
2021-05-05 21:11:23 -0400

    update

--- index.js
+++ index.js
@@ -1,44 +1,13 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+// TODO: Include packages needed for this application
 
+// TODO: Create an array of questions for user input
+const questions = [];
 
-//prompted for information about my application repository
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of application repository?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'describe your project:',
-      },
-    // {
-    //     type: 'input',
-    //     name: 'title',
-    //     message: 'What is the title?',
-    //   },
+// TODO: Create a function to initialize app
+function init() {}
 
-    // {
-    //   type: 'checkbox',
-    //   message: 'What languages do you know?',
-    //   name: 'stack',
-    //   choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-    // },
-    // {
-    //   type: 'list',
-    //   message: 'What is your preferred method of communication?',
-    //   name: 'contact',
-    //   choices: ['email', 'phone', 'telekinesis'],
-    // },
-  ])
-  .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
+// Function call to initialize app
+init();
